1815a4ca4ae3ec3d531671591ab622da12b0e083
rskumawat0143 <168708887+rskumawat0143@users.noreply.github.com>
2024-05-13 17:28:35 +0530

    Update ckeditor.js

--- js/ckeditor.js
+++ js/ckeditor.js
@@ -1,46 +1,42 @@
 
-var editElements = {};
-var editorDiv = document.createElement("div");
-$(editorDiv).addClass("ckeditor").attr("id",'editors');
-  $(editorDiv).appendTo('.editable');
-  let editors = document.querySelector('.editable')
-let editor = CKEDITOR.replace( editors, {
-extraPlugins: 'divarea'
-} );
+$(document).on('focus', '.editable', function () {
+  var editElements = {};
+  var id = $(this).attr('id');
+  var element = $(this);
+  var editor = CKEDITOR.replace(id, {
+    extraPlugins: 'divarea'
+  });
+  console.log(id);
+  editor.on('change', function () {
+    editElements[id] = true;
+  
+  });
 
-
-editors.on('change',function(){
-  editElements[$(this).attr('id')] = editor.getData();
-  // console.log(editElements)
-})
-
-editor.on('blur',function(){
-  saveData(editors)
+  editor.on('blur', function () {
+    var value = editor.getData();
+    saveData(element, value);
+  });
 });
 
+function saveData(editor, value) {
+  var id = editor.attr('id');
+  if (editElements[id] !== undefined) {
+    if (typeof saveChangesPopup !== 'undefined' && saveChangesPopup && !confirm('Save new changes?')) {
+      alert("Changes are not saved, you can continue to edit or refresh the page.");
+      return;
+    }
 
-function saveData(editor) {
-  if (editElements[editor.attr('id')]!=undefined) {
-      // Confirmation popup for saving changes (set in the database)
-      if (typeof saveChangesPopup !== 'undefined' && saveChangesPopup && !confirm('Save new changes?')) {
-          alert("Changed are not saved, you can continue to edit or refresh the page.");
-          return
-      }
-
-      var id = editor.attr('id');
-      var content = editElements[editor.attr('id')];
-      var target = (editor.attr('data-target')!=undefined) ? editor.attr('data-target'):'pages';
-      editElements[editor.attr('id')] = undefined;
-      $.post("",{
-          fieldname: id,
-          content: content,
-          target: target,
-          token: token,
-      })
-        .done(function() {
-            $("#save").show();
-            $('#save').delay(100).fadeOut();
-        });
+    var content = value;
+    var target = editor.data('target') || 'pages';
+    editElements[id] = undefined;
+    $.post("", {
+      fieldname: id,
+      content: content,
+      target: target,
+      token: token,
+    })
+    .done(function () {
+      $("#save").show().delay(100).fadeOut();
+    });
   }
 }
-// });
