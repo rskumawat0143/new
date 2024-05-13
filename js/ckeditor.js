ec856a4fbd2ab9aa1c9bd7c76d8aad04d1633a0e
rskumawat0143 <168708887+rskumawat0143@users.noreply.github.com>
2024-05-13 18:23:33 +0530

    Update ckeditor.js

--- js/ckeditor.js
+++ js/ckeditor.js
@@ -1,36 +1,52 @@
+var editElements = {};
 
-$(document).on('focus', '.editable', function () {
-  var editElements = {};
-  var id = $(this).attr('id');
-  var element = $(this);
-  var editor = CKEDITOR.replace(id, {
-    extraPlugins: 'divarea'
-  });
-  console.log(id);
-  editor.on('change', function () {
-    editElements[id] = true;
-  
-  });
-
-  editor.on('blur', function () {
-    var value = editor.getData();
-    saveData(element, value);
-  });
+var editor1 = CKEDITOR.replace('content', {
+  extraPlugins: 'divarea'
+});
+var editor2 = CKEDITOR.replace('subside', {
+  extraPlugins: 'divarea'
+});
+var editor3 = CKEDITOR.replace('footer', {
+  extraPlugins: 'divarea'
 });
 
-function saveData(editor, value) {
-  var id = editor.attr('id');
-  if (editElements[id] !== undefined) {
-    if (typeof saveChangesPopup !== 'undefined' && saveChangesPopup && !confirm('Save new changes?')) {
+editor1.on('change', handleEditorChange);
+editor1.on('blur', handleEditorBlur);
+editor2.on('change', handleEditorChange);
+editor2.on('blur', handleEditorBlur);
+editor3.on('change', handleEditorChange);
+editor3.on('blur', handleEditorBlur);
+
+function handleEditorChange(evt) {
+  var editorId = evt.editor.name;
+  editElements[editorId] = true;
+
+}
+
+function handleEditorBlur(evt) {
+  var editor = evt.editor;
+  var editorId = editor.name;
+  var elementId = editor.element.getId();
+  var element = $('#' + elementId);
+  var value = editor.getData();
+  saveData(element, value, editorId);
+  // editor.destroy()
+  
+}
+
+function saveData(element, value, editorId) {
+  if (editElements[editorId] !== undefined) {
+    var confirmSave = confirm('Save new changes?');
+    if (!confirmSave) {
       alert("Changes are not saved, you can continue to edit or refresh the page.");
       return;
     }
 
     var content = value;
-    var target = editor.data('target') || 'pages';
-    editElements[id] = undefined;
+    var target = element.data('target') || 'pages';
+    editElements[editorId] = undefined;
     $.post("", {
-      fieldname: id,
+      fieldname: editorId,
       content: content,
       target: target,
       token: token,
